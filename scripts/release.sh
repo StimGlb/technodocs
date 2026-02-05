31c385e18c628ff0fbd039337783aa265c27b883
stimglb <137829964+StimGlb@users.noreply.github.com>
2026-02-05 20:13:12 +0100

    Delete scripts directory

--- scripts/release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-ver=${1:-patch}
-npm test && npm run build
-npm version "$ver" -m "chore(release): %s"
-git push --follow-tags
-# Optionnel: générer changelog avec auto-changelog or conventional-changelog
