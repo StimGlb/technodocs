1b1db129076bf761853aeece858a9da7527e3c07
StimGlb <stimglb@gmail.com>
2026-04-03 12:45:12 +0200

    Auto-commit du 2026-04-03 12:42:40 (branch: dev)

--- src/js/wizard-config.js
+++ src/js/wizard-config.js
@@ -1,4 +1,4 @@
 // Centralized wizard configuration
 // Change the exported password here to update all forms at once.
 // NOTE: For production use consider a safer secret mechanism.
-export const CORRECT_PASSWORD = "lka9"; // À MODIFIER EN PRODUCTION
+export const CORRECT_PASSWORD = "2rv3"; // À MODIFIER EN PRODUCTION
